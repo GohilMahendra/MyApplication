35a25081b1ca2ccc4bdfe796c3c89a65b239afd8
yashraj Gupta <yashrajgupta@metafic.co>
2022-03-20 17:35:05 +0530

    add image sharing

--- app/src/main/java/com/example/myapplication/FeaturedProduct.java
+++ app/src/main/java/com/example/myapplication/FeaturedProduct.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,6 +13,13 @@ public class FeaturedProduct extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_featured_product);
 
+        Intent paramintent=getIntent();
+
+        String data=paramintent.getData().toString();
+
+
         TextView txt_prdName=findViewById(R.id.txt_productName);
+
+        txt_prdName.setText(data);
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,62 +20,85 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.FutureTarget;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.target.CustomViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.concurrent.ExecutionException;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txt_result=findViewById(R.id.txt_showResult);
-        Button btn_link=findViewById(R.id.btn_showlink);
 
-        ImageView img_product=findViewById(R.id.img_prdImage);
 
-        Glide.with(getApplicationContext()).load("https://m.media-amazon.com/images/I/41-ZuAdIKoL.jpg").into(img_product);
-        Uri uri=getIntent().getData();
 
-        if(uri!=null)
-        txt_result.setText(Uri.parse(String.valueOf(uri)).getQueryParameter("productName").toString());
+        String imagePath="https://www.mahendra.com/?productName=BlueShirt";
+        TextView txt_result = findViewById(R.id.txt_showResult);
+        Button btn_link = findViewById(R.id.btn_showlink);
+
+        ImageView img_product = findViewById(R.id.img_prdImage);
+
+        Glide.with(getApplicationContext()).load("https://m.media-amazon.com/images/I/41-ZuAdIKoL.jpg").diskCacheStrategy(DiskCacheStrategy.ALL).into(img_product);
+        Uri uri = getIntent().getData();
+
+        if (uri != null)
+            txt_result.setText(Uri.parse(String.valueOf(uri)).getQueryParameter("productName").toString());
 
         btn_link.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-                try {
-
-                    Intent shareintent = new Intent();
-                    shareintent.setAction(Intent.ACTION_SEND);
-                    shareintent.setType("text/*");
-                    shareintent.putExtra(Intent.EXTRA_TEXT, "https://www.mahendra.com/?productName=BlueShirt");
-                    //shareintent.setPackage("com.whatsapp");
-                    startActivity(Intent.createChooser(shareintent, "share via"));
-                    // is installed or not
-                    if (shareintent
-                            .resolveActivity(
-                                    getPackageManager())
-                            == null) {
-                        Toast.makeText(getApplicationContext(), "No Whatsapp Detected", Toast.LENGTH_LONG).show();
-                        return;
-                    }
-
-
-                }
-                catch(Exception e)
-                {
-                    Toast.makeText(getApplicationContext(),e.toString(), LENGTH_SHORT).show();
-                }
+                share();
             }
 
         });
 
 
-
-
-
     }
+
+
+    private  void share()
+    {
+        try {
+
+            Intent shareintent = new Intent();
+            shareintent.setAction(Intent.ACTION_SEND);
+            shareintent.setType("text/*");
+            shareintent.putExtra(Intent.EXTRA_TEXT, "https://www.mahendra.com/?productName=BlueShirt");
+
+
+            shareintent.putExtra(Intent.EXTRA_TITLE,"buy this");
+            //for image sharing
+
+          //  shareintent.putExtra(Intent.EXTRA_STREAM,yout_image_path);
+         //   shareintent.setType("image/png");
+
+
+            startActivity(Intent.createChooser(shareintent, "share via"));
+            // is installed or not
+            if (shareintent
+                    .resolveActivity(
+                            getPackageManager())
+                    == null) {
+                Toast.makeText(getApplicationContext(), "No Whatsapp Detected", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), e.toString(), LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
